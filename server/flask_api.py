ce1a469587430cd6e1b852a8c49913d1848fd61a
Hemal Pathak <hupathak904@gmail.com>
2025-04-20 16:50:09 -0500

    merging all backend into flask server

--- server/flask_api.py
+++ server/flask_api.py
@@ -1,7 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify 
+import requests
+import urllib.parse
 from ml_model import classify_message
+from api_key import key
+from api_key import ipqs_key
 
 app = Flask(__name__)
+API_KEY = key
+IPQS_API_KEY = ipqs_key
 
 @app.route('/makePrediction', methods=['POST'])
 def makePrediction():
@@ -13,6 +19,46 @@ def makePrediction():
   prediction = classify_message([message])
   return jsonify(prediction), 200
 
+
+
+@app.route('/verifyURL', methods=['POST'])
+def verifyURL():
+  data = request.get_json()
+  inputURL = data.get('url')
+  api_params = {
+    "client": {
+          "clientId": "fraudfilter",
+          "clientVersion": "1.0"
+        },
+        "threatInfo": {
+          "threatTypes": ["MALWARE", "SOCIAL_ENGINEERING"],
+          "platformTypes": ["ANY_PLATFORM"],
+          "threatEntryTypes": ["URL"],
+          "threatEntries": [{ "url": inputURL }]
+        }
+  }
+
+  try:
+    api_response = requests.post(f'https://safebrowsing.googleapis.com/v4/threatMatches:find?key={API_KEY}', json=api_params)
+    return api_response.json(), 200
+  except Exception as e:
+    print("Safe Browsing API Error:", str(e))
+    return jsonify({'error': 'Safe Browsing API call not successful'}), 400
+
+
+@app.route('/verifyURLUpgraded', methods=['POST'])
+def verifyURLUpgraded():
+  data = request.get_json()
+  inputURL = data.get('url')
+  encodedURL = urllib.parse.quote_plus(inputURL) 
+
+  try:
+    ipqs_response = requests.post(f'https://www.ipqualityscore.com/api/json/url/{IPQS_API_KEY}?url={encodedURL}')
+    return ipqs_response.json(), 200
+  except Exception as e:
+    print("IPQS API Error:", str(e))
+    return jsonify({'error': 'IPQS API call not successful'}), 400
+
 if __name__ == "__main__":
   print("Flask API endpoint connected")
-  app.run(port=5001)
+  app.run(port=5000)
--- server/index.js
+++ server/index.js
@@ -1,20 +1,18 @@
 import express from "express";
 import cors from "cors";
 import axios from "axios";
-import {key} from "./api.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 
-const API_KEY = key;
 
-// Completing POST request to Google Safebrowsing API
+// POST request to Google Safe Browsing API
 app.post("/api/verifyURL", async (req, res) => {
   const { inputUrl } = req.body;
   if (!inputUrl) {
-    return res.status(400).json({ error: "Input data is required" });
+    return res.status(400).json({ error: "Safe Browsing API -Input data is required" });
   }
 
   // Requesting Google Safe Browsing API
@@ -34,25 +32,46 @@ app.post("/api/verifyURL", async (req, res) => {
       });
       return res.status(200).json(httpResponse.data);
   } catch {
-    return res.status(400).json({error: "API call not successful"});
+    return res.status(400).json({error: "Safe Browsing API call not successful"});
   }
 });
 
 
-// Completing POST request to ML model for message classification
+// GET request to Google Web Risk API
+app.get("/api/verifyURLExtended", async (req, res) => {
+  const { inputUrl } = req.query;
+  if (!inputUrl) {
+    return res.status(400).json({ error: "Web Risk API - Input data is required" });
+  }
+
+  // Requesting Google Web Risk API
+  try{
+    const encodedUrl = encodeURIComponent(inputUrl);
+    const apiEndpoint = `https://webrisk.googleapis.com/v1/uris:search?threatTypes=MALWARE&threatTypes=SOCIAL_ENGINEERING&threatTypes=UNWANTED_SOFTWARE&threatTypes=SOCIAL_ENGINEERING_EXTENDED_COVERAGE&uri=${encodedUrl}&key=${API_KEY}`
+    const apiResponse = await axios.get(apiEndpoint);
+    return res.status(200).json(apiResponse.data);
+  } catch (err) {
+    console.error("Web Risk API Error:", err.response?.data || err.message);
+    return res.status(400).json({error: "Web Risk API call not successful"});
+  }
+});
+
+
+// POST request to ML model for message classification
 app.post("/api/classify", async (req, res) => {
   const { inputData } = req.body;
   if (!inputData) {
-    return res.status(400).json({ error: "Input message is required" });
+    return res.status(400).json({ error: "Flask API - Input message is required" });
   }
 
   // Request to ML model using Flask
   try{
-    const modelResponse = await axios.post(`http://127.0.0.1:5001/makePrediction`, {text: inputData});
+    const modelResponse = await axios.post(`http://127.0.0.1:5000/makePrediction`, {text: inputData});
     return res.status(200).json(modelResponse.data);
-  } catch {
-    return res.status(400).json({error: "API call not successful"});
+  } catch (err) {
+    console.error("Web Risk API Error:", err.response?.data || err.message);
+    return res.status(400).json({error: "Flask API call not successful"});
   }
 });
 
-app.listen(5000, () => console.log("server running on port 5000"));
+app.listen(5000, () => console.log("Server running on port 5000"));
