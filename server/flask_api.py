dcfd9c5d7b38619edb3162b2ce70050bfcb90f78
Hemal Pathak <hupathak904@gmail.com>
2025-04-22 10:32:35 -0500

    updated UI to add confidence score and info icon

--- server/flask_api.py
+++ server/flask_api.py
@@ -10,22 +10,31 @@ load_dotenv()
 API_KEY = os.getenv("SB_API_KEY")
 IPQS_API_KEY = os.getenv("IPQS_API_KEY")
 
+
+
+# API call to the ML model to classify the message as spam or not spam
 @app.route('/makePrediction', methods=['POST'])
 def makePrediction():
+  # Get user input
   data = request.get_json()
   message = data.get('text')
   if not message:
     return jsonify({'error': 'Input data is required'}), 400
   
+  # Classify user input using ML Model
   prediction = classify_message([message])
   return jsonify(prediction), 200
 
 
 
+# API call to Google Safe Browsing API to determine if URL is safe
 @app.route('/verifyURL', methods=['POST'])
 def verifyURL():
+  # Get user input
   data = request.get_json()
   inputURL = data.get('url')
+
+  # Define input for API call
   api_params = {
     "client": {
           "clientId": "fraudfilter",
@@ -39,6 +48,7 @@ def verifyURL():
         }
   }
 
+  # Make POST request
   try:
     api_response = requests.post(f'https://safebrowsing.googleapis.com/v4/threatMatches:find?key={API_KEY}', json=api_params)
     return api_response.json(), 200
@@ -47,12 +57,18 @@ def verifyURL():
     return jsonify({'error': 'Safe Browsing API call not successful'}), 400
 
 
+
+# API call to IP Quality Score URL Scanner to evaluate URL (more advanced than Google Safe Browsing or Web Risk APIs)
 @app.route('/verifyURLUpgraded', methods=['POST'])
 def verifyURLUpgraded():
+  # Get user input
   data = request.get_json()
   inputURL = data.get('url')
+
+  # Encode URL
   encodedURL = urllib.parse.quote_plus(inputURL) 
 
+  # Make POST request
   try:
     ipqs_response = requests.post(f'https://www.ipqualityscore.com/api/json/url/{IPQS_API_KEY}?url={encodedURL}')
     return ipqs_response.json(), 200
@@ -60,6 +76,9 @@ def verifyURLUpgraded():
     print("IPQS API Error:", str(e))
     return jsonify({'error': 'IPQS API call not successful'}), 400
 
+
+
+# Run Flask Server
 if __name__ == "__main__":
   print("Flask API endpoint connected")
   app.run(port=5000)
